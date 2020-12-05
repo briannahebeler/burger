c9ca64db923c853f4f23076bdc91f9aed3474da9
Brianna Hebeler <briannahebeler@gmail.com>
2020-12-05 18:56:37 -0500

    coded server.js

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 const express = require("express");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function () {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
