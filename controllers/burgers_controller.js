f954476bc305e2d83f36a01b56adc006902280f4
Brianna Hebeler <briannahebeler@gmail.com>
2020-12-05 18:52:20 -0500

    added code for curgers_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,6 +1,50 @@
 const express = require("express");
-const burger = require("../models/burger");
+const router = express.Router();
+const burger = require("../models/burger.js");
 
-//Create the router for the app//
+router.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
-module.exports = burgers_controller;
+router.get("/burgers", function (req, res) {
+    burger.all(function (data) {
+        res.json({ burgers: data });
+    });
+});
+
+router.post("/burgers", function (req, res) {
+    burger.create([
+        "name", "devoured"
+    ], [
+        req.body.name, req.body.devoured
+    ], function (result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.json({ id: req.params.id });
+        }
+    });
+});
+
+router.delete("/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+    burger.delete(condition, function (result) {
+        if (result.affectedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
