22fe3ab5fe81440e705fb1d97eca879d1c44d143
Brianna Hebeler <briannahebeler@gmail.com>
2020-12-05 01:37:05 -0500

    created inquiries in seeds sql

---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+insert into burgers (burger_name) values ("Big Mac"), ("Whopper"), ("Impossible Burger");
+insert into burgers (burger_name, devoured) values ("Turkey Burger", true), ("Bubba Burger", true), ("Barbeque Burger", true);
