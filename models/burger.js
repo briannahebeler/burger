daad13279f10607abe77af76c396108394bd5f8b
Brianna Hebeler <briannahebeler@gmail.com>
2020-12-05 18:54:23 -0500

    added burger.js code to model

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,26 @@
 const orm = require("../config/orm.");
 
-// Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM. //
+var burger = {
+    all: function (cb) {
+        orm.all("burgers", function (res) {
+            cb(res);
+        });
+    },
+    create: function (cols, vals, cb) {
+        orm.create("burgers", cols, vals, function (res) {
+            cb(res);
+        });
+    },
+    update: function (objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function (res) {
+            cb(res);
+        });
+    },
+    delete: function (condition, cb) {
+        orm.delete("burgers", condition, function (res) {
+            cb(res);
+        });
+    }
+};
 
-module.exports = burgers;
+module.exports = burger;
