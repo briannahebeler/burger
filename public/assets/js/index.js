d199be64931632962b5dbaa0943e42384899f779
Brianna Hebeler <briannahebeler@gmail.com>
2020-12-05 19:29:04 -0500

    build public files modeled after catApp

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -8,3 +8,106 @@
 
 // on submit what user types in textbox shows dynamically on the left list along with a button in the middle col //
 // on click of the devour button the listed item displays on right column (btn) disappears
+
+$(function() {
+  $.ajax("/burgers", {
+    type: "GET"
+  }).then(function(data) {
+    var devouredElem = $("#devouredBurgers");
+    var nodevouredElem = $("#notDevouredBurgers");
+
+    var burgers = data.burgers;
+    var len = burgers.length;
+
+    for (var i = 0; i < len; i++) {
+      var new_elem =
+        "<li>" +
+        burgers[i].id + 
+        ". "+burgers[i].name +
+        "<button class='change-devour' data-id='" +
+        burgers[i].id +
+        "' data-newdevour='" +
+        !burgers[i].devoured +
+        "'>";
+
+      if (burgers[i].devoured) {
+        new_elem += "DEVOUR TIME!";
+      } else {
+        new_elem += "WAKE UP!";
+      }
+
+      new_elem += "</button>";
+
+      new_elem +=
+        "<button class='delete-burger' data-id='" +
+        burgers[i].id +
+        "'>DELETE!</button></li>";
+
+      if (burgers[i].devoured) {
+        devouredElem.append(new_elem);
+      } else {
+        nodevouredElem.append(new_elem);
+      }
+    }
+  });
+
+  $(document).on("click", ".change-devour", function(event) {
+    var id = $(this).data("id");
+    var newDevour = $(this).data("newdevour")===true;
+
+    var newDevourState = {
+      devoured: newDevour
+    };
+
+    // Send the PUT request.
+    $.ajax("/burgers/" + id, {
+      type: "PUT",
+      data: JSON.stringify(newDevourState),
+      dataType:'json',
+      contentType: 'application/json'
+    }).then(function() {
+      console.log("changed devour to", newDevour);
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#ca")
+        .val()
+        .trim(),
+      devoured: $("[name=devoured]:checked")
+        .val()
+        .trim()
+    };
+
+    // Send the POST request.
+    $.ajax("/burgers", {
+      type: "POST",
+      data: JSON.stringify(newBurger),
+      dataType:'json',
+      contentType: 'application/json'
+    }).then(function() {
+      console.log("created new burger");
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+
+  $(document).on("click", ".delete-burger", function(event) {
+    var id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax("/burgers/" + id, {
+      type: "DELETE"
+    }).then(function() {
+      console.log("deleted burger", id);
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+});
