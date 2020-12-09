65f850f320e5c520f729fb464657d02f187502ed
Brianna Hebeler <briannahebeler@gmail.com>
2020-12-09 11:58:20 -0500

    psuedo code and reformat server js

--- server.js
+++ server.js
@@ -4,15 +4,18 @@ var PORT = process.env.PORT || 8000;
 
 var app = express();
 
+// serve static content for app from the public folder // 
 app.use(express.static("public"));
 
+// parse app body as JSON //
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// import routes & give the server access //
 var routes = require("./controllers/burgers_controller.js");
-
 app.use(routes);
 
+// start server //
 app.listen(PORT, function () {
     console.log("Server listening on: http://localhost:" + PORT);
 });
