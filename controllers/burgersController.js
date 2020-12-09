bf908a81c35127952435a401091c1d2307ccc8f0
Brianna Hebeler <briannahebeler@gmail.com>
2020-12-09 15:12:45 -0500

    getting rid of extra html

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -4,47 +4,47 @@ var router = express.Router();
 
 var burger = require("../models/burger.js");
 
-router.get("/", function (req, res) {
+router.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-router.get("/burgers", function (req, res) {
-  burger.all(function (data) {
+router.get("/burgers", function(req, res) {
+  burger.all(function(data) {
     res.json({ burgers: data });
   });
 });
 
-router.post("/burgers", function (req, res) {
+router.post("/burgers", function(req, res) {
   burger.create([
     "name", "devoured"
   ], [
     req.body.name, req.body.devoured
-  ], function (result) {
+  ], function(result) {
     res.json({ id: result.insertId });
   });
 });
 
-router.put("/burgers/:id", function (req, res) {
+router.put("/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
   burger.update({
     devoured: req.body.devoured
-  }, condition, function (result) {
+  }, condition, function(result) {
     if (result.changedRows == 0) {
       // if no rows were changed, then id does not exist, so 404 //
       return res.status(404).end();
     } else {
-      res.json({ id: req.params.id });
+      res.json({ id: req.params.id});
     }
   });
 });
 
-router.delete("/burgers/:id", function (req, res) {
+router.delete("/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  burger.delete(condition, function (result) {
+  burger.delete(condition, function(result) {
     if (result.affectedRows == 0) {
       // if no rows were changed, then id does not exist, so 404 //
       return res.status(404).end();
